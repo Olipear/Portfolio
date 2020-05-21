5b3a300a650c3b7c8796168d1285d508e07b690b
Olipear <olipear@gmail.com>
2020-05-21 10:50:35 +0100

    updating before rename

---
+++ src/components/Splash.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../img/logo.svg'
+
+const Splash = ({SplashInfo}) => {
+
+    return (
+        <div className="splash">
+            
+            {SplashInfo}
+        </div>
+      )
+}
+
+export default Splash
