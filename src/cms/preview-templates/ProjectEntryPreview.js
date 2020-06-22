a4732314a5a6c4c06c29eb662c23ba3ad4214927
Olipear <olipear@gmail.com>
2020-06-22 17:55:55 +0100

    update template for CMS

--- src/cms/preview-templates/ProjectEntryPreview.js
+++ src/cms/preview-templates/ProjectEntryPreview.js
@@ -6,10 +6,7 @@ const ProjectEntryPreview = ({ entry, widgetFor }) => {
   const tags = entry.getIn(['data', 'tags'])
   return (
     <ProjectEntryTemplate
-      content={widgetFor('body')}
-      description={entry.getIn(['data', 'description'])}
-      tags={tags && tags.toJS()}
-      title={entry.getIn(['data', 'title'])}
+      content={entry.toJS()}
     />
   )
 }
