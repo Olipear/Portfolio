2b55e52352a8f67c14fae16cf5521e9a0f6288f4
Olipear <olipear@gmail.com>
2020-07-02 16:28:28 +0100

    interaction styles

--- src/components/navbar/NavbarContent.js
+++ src/components/navbar/NavbarContent.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, graphql, useStaticQuery } from "gatsby";
 import NavMenu from "./NavMenu";
 import NavMenuButton from "./NavMenuButton";
 import logo from "../../img/logo.svg";
 import NavOverlay from "./NavOverlay";
-
+import { motion } from "framer-motion";
 
 const NavbarContent = ({ splash }) => {
-  const data = useStaticQuery(navigationQuery).markdownRemark.frontmatter.navigation;
+  const data = useStaticQuery(navigationQuery).markdownRemark.frontmatter
+    .navigation;
 
   const [open, setOpen] = useState(false);
 
@@ -15,32 +16,34 @@ const NavbarContent = ({ splash }) => {
     <div className="container">
       <NavOverlay open={open} />
       <div className="navbar-brand">
-        <Link to="/" className="navbar-item" title="home">
-          <img src={logo} style={{height: "100%"}} alt="olipear" />
+        <Link to="/" title="home" className="navbar-item">
+          <motion.div style={{height: "100%"}} whileHover={{ scale: 1.1}}>
+            <img src={logo} style={{ height: "100%" }} alt="olipear" />
+          </motion.div>
         </Link>
       </div>
-      
+
       <NavMenu data={data} open={open} setOpen={setOpen} />
       <NavMenuButton open={open} setOpen={setOpen} />
     </div>
-  )  
+  );
 };
 
 export default NavbarContent;
 
 const navigationQuery = graphql`
-query navigationQuery {
-  markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-    frontmatter {
-      navigation {
-        githuburl
-        githublabel
-        linkedinurl
-        linkedinlabel
-        cv_pdf
-        cv_pdflabel
+  query navigationQuery {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      frontmatter {
+        navigation {
+          githuburl
+          githublabel
+          linkedinurl
+          linkedinlabel
+          cv_pdf
+          cv_pdflabel
+        }
       }
     }
   }
-}
-`
+`;
